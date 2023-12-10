0c9afc5c6c2fa29ec930d641fc63fe2de3cb3c45
Murilo Henrique <murilohenrique8508@gmail.com>
2023-12-10 13:18:40 -0300

    teste

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://github.com/muriloh2001/js-developer-portfolio/blob/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
